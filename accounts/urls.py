1bb23ef0620d06884a7c9128f80621cf79208873
imkaoru <imkaoru.s@gmail.com>
2021-12-04 15:42:58 +0900

    Userモデルの統一、カードの3段組

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = "accounts"
 urlpatterns = [
     path("login/", LoginView.as_view(
-        redirect_authenticated_user=False, ## 後でTrueに変更
+        redirect_authenticated_user=False, #後でTrueに変更
         template_name="accounts/login.html"
     ), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- diary/views.py
+++ diary/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 import datetime
 
 from diary.models import Diary
+
 
 
 # def index(request):
@@ -14,11 +16,24 @@ from diary.models import Diary
 #         "newyear": now.month == 1 and now.day == 1
 #     })
 
+
+# @login_required
+# def testview(request):
+#     user = request.user
+
+
+@login_required
 def index(request):
     return render(request, "diary/index.html")
 
 
+@login_required
 def myDiary(request):
+    login_user = request.user
+    # print(login_user)
     diaries = Diary.objects.all()
-    context = {"diaries": diaries}
+    context = {
+        "login_user": login_user,
+        "diaries": diaries
+    }
     return render(request, "diary/my_diary.html", context)
