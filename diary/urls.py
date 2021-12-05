7872b6ceb24671298f4e8c7a160e271505e07cd4
imkaoru <imkaoru.s@gmail.com>
2021-12-05 22:20:54 +0900

    Sign up機能の実装

--- diary/urls.py
+++ diary/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "diary"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("my_diary", views.myDiary, name="my_diary"),
+    path("my_diary/", views.myDiary, name="my_diary"),
     path('update_check_status/<int:diary_id>/', views.update_check_status, name='update_check_status'),
 ]
