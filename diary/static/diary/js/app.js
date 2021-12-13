01a16c2f7ce87ed7a6fb0be2e66fdacad5f12041
imkaoru <imkaoru.s@gmail.com>
2021-12-14 02:12:09 +0900

    モーダルウィンドウ実装(レイアウト含)、speechToText機能追加

--- diary/static/diary/js/app.js
+++ diary/static/diary/js/app.js
@@ -25,15 +25,14 @@ function previewImage(obj) {
 
 //////////////////// my_diary.html
 // 音声合成
-// 現状、最後まで聞いた場合再度再生するために2回押さないといけない
-let switchOver = 0;
+let switchOverS = 0;
 function textToSpeech(count) {
     if (!speechSynthesis.speaking) {
-        switchOver = 0;
+        switchOverS = 0;
     }
-    switch (switchOver) {
+    switch (switchOverS) {
         case 0:
-            switchOver = 1;
+            switchOverS = 1;
             let id = 'card-text' + count;
             let word = document.getElementById(id).innerHTML;
             let u = new SpeechSynthesisUtterance();
@@ -42,8 +41,84 @@ function textToSpeech(count) {
             speechSynthesis.speak(u);
             break;
         case 1:
-            switchOver = 0;
+            switchOverS = 0;
             speechSynthesis.cancel();
+            break;
+    }
+}
+
+// モーダルウィンドウ
+$(".speechToText").modaal({
+	overlay_close:true, //モーダル背景クリック時に閉じるか
+	before_open:function(){ //モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden'); /*縦スクロールバーを出さない*/
+	},
+	after_close:function(){ //モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll'); /*縦スクロールバーを出す*/
+	}
+});
+
+// 音声認識
+SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+let recognition = new SpeechRecognition();
+
+recognition.lang = 'en-US';
+recognition.interimResults = true;
+recognition.continuous = true;
+
+recognition.onresult = (event) => {
+    let btnId = 'recording-btn' + modaalNum;
+    let recordingBtn = document.getElementById(btnId);
+
+    let resultId = 'result-div' + modaalNum;
+    let resultDiv = document.getElementById(resultId);
+    let preResult = resultDiv.firstElementChild;
+
+    let finalTranscript = ''; // 確定した認識結果
+    let interimTranscript = ''; // 暫定の認識結果
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+        let transcript = event.results[i][0].transcript;
+
+        let p = document.createElement("p");
+        // resultDiv.appendChild(p);
+        resultDiv.firstElementChild.after(p);
+
+        if (event.results[i].isFinal) {
+            finalTranscript = transcript;
+            preResult.innerHTML = finalTranscript;
+            p.innerText = finalTranscript;
+        } else {
+            interimTranscript = transcript;
+            preResult.innerHTML = '<p class="standby">' + interimTranscript + '</p>';
+        }
+    }
+
+    recognition.onaudioend = () => {
+        switchOverR = 0;
+        recordingBtn.innerHTML = '<i class="fas fa-microphone fa-2x"></i>';
+        resultDiv.innerHTML = '<p class="standby">Speak into the microphone. (English)</p>';
+    }
+}
+
+let modaalNum = 0;
+let switchOverR = 0;
+
+function speechToText(count) {
+    modaalNum = count;
+    let id = 'recording-btn' + modaalNum;
+    let recordingBtn = document.getElementById(id);
+
+    switch (switchOverR) {
+        // recordingBtn.classList.add("foo");
+        case 0:
+            switchOverR = 1;
+            recordingBtn.innerHTML = '<i class="far fa-stop-circle fa-2x"></i>';
+            recognition.start();
+            break;
+        case 1:
+            switchOverR = 0;
+            recordingBtn.innerHTML = '<i class="fas fa-microphone fa-2x"></i>';
+            recognition.stop();
             break;
     }
 }
